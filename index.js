c8504371fb38e4d8f040a57cd9ad01addd7aba5b
Jeff Glanville <jeff.glanville@outlook.com>
2020-08-10 19:14:13 -0500

    update index.js, add middleware file

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express")
+const morgan = require("morgan")
+const logger = require("./middleware/logger")
 const welcomeRouter = require("./welcome/welcome-router")
 const usersRouter = require("./users/users-router")
+
 
 const server = express()
 const port = 4000
 
 server.use(express.json())
+// server.use(morgan("combined"))
+
+server.use(logger())
 
 server.use(welcomeRouter)
 server.use(usersRouter)
